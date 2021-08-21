b03cae26e1b152c7d598d30aa3a46ba05155b4af
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2021-08-21 06:52:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,91 +15,91 @@ const NotFound = lazy(() => import('./component/not-found'));
 
 export default class App extends Component {
 	state = {
-		products: productList
+	  products: productList
 	};
 
 	getIndexOfSelectedProduct = (product) => {
-		return [ ...this.state.products ].indexOf(product);
+	  return [ ...this.state.products ].indexOf(product);
 	};
 
 	handleReset = () => {
-		const products = this.state.products.map((item) => {
-			item.value = 0;
-			return item;
-		});
-		this.setState({ products });
+	  const products = this.state.products.map((item) => {
+	    item.value = 0;
+	    return item;
+	  });
+	  this.setState({ products });
 	};
 
 	handleDelete = (product) => {
-		const index = this.getIndexOfSelectedProduct(product);
-		const products = [ ...this.state.products.slice(0, index), ...this.state.products.slice(index + 1) ];
-		this.setState({ products });
+	  const index = this.getIndexOfSelectedProduct(product);
+	  const products = [ ...this.state.products.slice(0, index), ...this.state.products.slice(index + 1) ];
+	  this.setState({ products });
 	};
 
 	handleIncrement = (product) => {
-		const index = this.getIndexOfSelectedProduct(product);
-		const value = product.value + 1;
-		const products = [
-			...this.state.products.slice(0, index),
-			{ ...product, value },
-			...this.state.products.slice(index + 1)
-		];
-		this.setState({ products });
+	  const index = this.getIndexOfSelectedProduct(product);
+	  const value = product.value + 1;
+	  const products = [
+	    ...this.state.products.slice(0, index),
+	    { ...product, value },
+	    ...this.state.products.slice(index + 1)
+	  ];
+	  this.setState({ products });
 	};
 
 	handleDecrement = (product) => {
-		const index = this.getIndexOfSelectedProduct(product);
-		const value = product.value - 1 <= 0 ? 0 : product.value - 1;
-		const products = [
-			...this.state.products.slice(0, index),
-			{ ...product, value },
-			...this.state.products.slice(index + 1)
-		];
-		this.setState({ products });
+	  const index = this.getIndexOfSelectedProduct(product);
+	  const value = product.value - 1 <= 0 ? 0 : product.value - 1;
+	  const products = [
+	    ...this.state.products.slice(0, index),
+	    { ...product, value },
+	    ...this.state.products.slice(index + 1)
+	  ];
+	  this.setState({ products });
 	};
 
 	render() {
-		return (
-			<Fragment>
-				<Router>
-					<Navigator products={this.state.products} />
-					<div className="App" style={{ marginTop: 25 }}>
-						<main className="container">
-							<Suspense fallback={<div>Loading...</div>}>
-								<Switch>
-									<Route path="/checkout">
-										<Checkout products={this.state.products} />
-									</Route>
-									<Route path="/about">
-										<About />
-									</Route>
-									<Route path="/contact">
-										<Contact />
-									</Route>
-									<Route
-										path="/detail/:id"
-										render={(props) => <Detail products={this.state.products} {...props} />}
-									/>
-									<Route exact path="/">
-										<Products
-											onAddItem={this.handleIncrement}
-											onRemoveItem={this.handleDecrement}
-											onDelete={this.handleDelete}
-											onReset={this.handleReset}
-											products={this.state.products}
-										/>
-									</Route>
-									<Route path="/not-found" component={NotFound} />
-									<Redirect from="*" to="/not-found" />
-									<Route path="/*">
-										<NotFound />
-									</Route>
-								</Switch>
-							</Suspense>
-						</main>
-					</div>
-				</Router>
-			</Fragment>
-		);
+	  return (
+	    <Fragment>
+	      <Router>
+	        <Navigator products={this.state.products} />
+	        <div className="App" style={{ marginTop: 25 }}>
+	          <main className="container">
+	            <Suspense fallback={<div>Loading...</div>}>
+	              <Switch>
+	                <Route path="/checkout">
+	                  <Checkout products={this.state.products} />
+	                </Route>
+	                <Route path="/about">
+	                  <About />
+	                </Route>
+	                <Route path="/contact">
+	                  <Contact />
+	                </Route>
+	                <Route
+	                  path="/detail/:id"
+	                  render={(props) => <Detail products={this.state.products} {...props} />}
+	                />
+	                <Route exact path="/">
+	                  <Products
+	                    onAddItem={this.handleIncrement}
+	                    onRemoveItem={this.handleDecrement}
+	                    onDelete={this.handleDelete}
+	                    onReset={this.handleReset}
+	                    products={this.state.products}
+	                  />
+	                </Route>
+	                <Route path="/not-found" component={NotFound} />
+	                <Redirect from="*" to="/not-found" />
+	                <Route path="/*">
+	                  <NotFound />
+	                </Route>
+	              </Switch>
+	            </Suspense>
+	          </main>
+	        </div>
+	      </Router>
+	    </Fragment>
+	  );
 	}
 }
